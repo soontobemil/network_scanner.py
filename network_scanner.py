7ff844d62687291bc5440338e8f3e5a96c52cd17
Jay Sim <jay.sim@vshn.net>
2023-03-03 20:54:44 -0800

    ubuntu

--- network_scanner.py
+++ network_scanner.py
@@ -6,8 +6,7 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcst = broadcast/arp_request
-    answered, unanswered = scapy.srp(arp_request_broadcst) #sr = send and receive p=custom packet
-
-
-
+    answered, unanswered = scapy.srp(arp_request_broadcst, timeout=1) #sr = send and receive p=custom packet
+    print(answered.summary())
+    # print(answered)
 scan("192.168.64.1/24")
