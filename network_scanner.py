93c22de8e4e085ac79a83969ad3ac80e185c46f9
Jay Sim <jay.sim@vshn.net>
2023-03-07 21:26:26 -0800

    Added clients list

--- network_scanner.py
+++ network_scanner.py
@@ -6,7 +6,13 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcst = broadcast/arp_request
-    answered, unanswered = scapy.srp(arp_request_broadcst, timeout=1) #sr = send and receive p=custom packet
-    print(answered.summary())
-    # print(answered)
-scan("192.168.64.1/24")
+    answered_list = scapy.srp(arp_request_broadcst, timeout=1, verbose=False)[0]
+
+    clients_list = []
+    for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+        print(element[1].psrc + element[1].hwsrc)
+    print(clients_list)
+
+scan("192.168.1.1/24")
